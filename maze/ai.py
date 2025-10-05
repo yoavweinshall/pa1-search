fab6c25c4c979cfe71f807cb45db1e5cd991d377
yoavweinshall <yoavweinshall@gmail.com>
2025-10-05 09:55:11 -0700

    add type hints and implement distance methods

--- maze/ai.py
+++ maze/ai.py
@@ -1,8 +1,14 @@
 from collections import deque
 import heapq, math
+from typing import Tuple
+
 from common import plugins
+from .game import Maze
 
-def bfs(maze):
+ROW_LOCATION_IN_POSITION_TUPLE = 0
+COLUMN_LOCATION_IN_POSITION_TUPLE = 1
+
+def bfs(maze: Maze):
     start, goal = maze.start, maze.goal
     path = []
     expanded = set()
@@ -13,7 +19,7 @@ def bfs(maze):
     return path, expanded
 
 
-def dfs(maze):
+def dfs(maze: Maze):
     start, goal = maze.start, maze.goal
     path = []
     expanded = set()
@@ -23,7 +29,8 @@ def dfs(maze):
 
     return path, expanded
 
-def dijkstra(maze):
+
+def dijkstra(maze: Maze):
     start, goal = maze.start, maze.goal
     path = []
     expanded = set()
@@ -33,15 +40,28 @@ def dijkstra(maze):
 
     return path, expanded
 
-def manhattan_distance(a, b):
-    # TODO
-    return None
+def manhattan_distance(a: Tuple[int, int], b: Tuple[int, int]) -> float:
+    """
+    calculates manhattan distance between to cells
+    :param a: position of first cell in a (row,column) tuple
+    :param b: position of second cell in a (row,column) tuple
+    :return: The manhattan distance between two cells defined to be |a.x - b.x| + |a.y - b.y|
+    """
+    return math.sqrt((a[ROW_LOCATION_IN_POSITION_TUPLE] - b[ROW_LOCATION_IN_POSITION_TUPLE]) ** 2 +
+                     (a[COLUMN_LOCATION_IN_POSITION_TUPLE] - b[COLUMN_LOCATION_IN_POSITION_TUPLE]) ** 2)
 
-def euclidean_distance(a, b):
-    # TODO
-    return None
 
-def a_star(maze, heuristic):
+def euclidean_distance(a: Tuple[int, int], b: Tuple[int, int]) -> float:
+    """
+    calculates Euclidean distance between two cells
+    :param a: position of first cell in a (row,column) tuple
+    :param b: position of second cell in a (row,column) tuple
+    :return: The Euclidian distance between two cells defined to be sqrt((a.x - b.x)^2 + (a.y - b.y)^2)
+    """
+    return (abs(a[ROW_LOCATION_IN_POSITION_TUPLE] - b[ROW_LOCATION_IN_POSITION_TUPLE]) +
+            abs(a[COLUMN_LOCATION_IN_POSITION_TUPLE] - b[COLUMN_LOCATION_IN_POSITION_TUPLE]))
+
+def a_star(maze: Maze, heuristic):
     start, goal = maze.start, maze.goal
     path = []
     expanded = set()
@@ -51,7 +71,7 @@ def a_star(maze, heuristic):
 
     return path, expanded
 
-def gbfs(maze, heuristic):
+def gbfs(maze: Maze, heuristic):
     start, goal = maze.start, maze.goal
     path = []
     expanded = set()
